19695e43ed17126d63e34462a1f732ff28b4cef1
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-12 17:19:30 +0200

    added ui elements to toggle editing and displaying the player name

--- src/components/Player/Player.component.jsx
+++ src/components/Player/Player.component.jsx
@@ -1,11 +1,27 @@
+import { useState } from 'react';
+
 function Player({ name, symbol }) {
+	const [isEdit, setIsEdit] = useState(false);
+
+	function handleClick() {
+		setIsEdit((val) => !val);
+	}
+
+	let displaySetName = <span className='player-name'>{name}</span>;
+	let buttonText = 'Edit';
+
+	if (isEdit) {
+		displaySetName = <input type='text' required />;
+		buttonText = 'Save';
+	}
+
 	return (
 		<li>
 			<span className='player'>
-				<span className='player-name'>{name}</span>
+				{displaySetName}
 				<span className='player-symbol'>{symbol}</span>
 			</span>
-			<button>Edit</button>
+			<button onClick={handleClick}>{buttonText}</button>
 		</li>
 	);
 }
