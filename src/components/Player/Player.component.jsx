6b052e97d9376a39194cb4181a035d9319423003
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-12 19:23:47 +0200

    used two-way binding to update the controlled input value

--- src/components/Player/Player.component.jsx
+++ src/components/Player/Player.component.jsx
@@ -1,17 +1,22 @@
 import { useState } from 'react';
 
 function Player({ name, symbol }) {
+	const [playerName, setPlayerName] = useState(name);
 	const [isEdit, setIsEdit] = useState(false);
+
+	function handleChange(event) {
+		setPlayerName(event.target.value)
+	}
 
 	function handleClick() {
 		setIsEdit((val) => !val);
 	}
 
-	let displaySetName = <span className='player-name'>{name}</span>;
+	let displaySetName = <span className='player-name'>{playerName}</span>;
 	let buttonText = 'Edit';
 
 	if (isEdit) {
-		displaySetName = <input type='text' required value={name} />;
+		displaySetName = <input type='text' required value={playerName} onChange={handleChange} />;
 		buttonText = 'Save';
 	}
 
