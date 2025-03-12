dc50612a7153947705825906634145eb5d1d1a4f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-03-12 12:21:16 +0200

    first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import './App.css';
 
 function App() {
-	return (<p>coming soon...</p>);
+	return (
+		<main>
+			<div id='game-container'>
+				<ol id='players'>
+					<li>
+						<span className='player-name'>Player 1</span>
+						<span className='player-symbol'>X</span>
+					</li>
+					<li>
+						<span className='player-name'>Player 2</span>
+						<span className='player-symbol'>O</span>
+					</li>
+				</ol>
+				GAMEBOARD
+			</div>
+			LOG
+		</main>
+	);
 }
 
 export default App;
